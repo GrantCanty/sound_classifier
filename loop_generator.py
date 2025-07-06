094f249bc886c844196acb44299188112a337614
Grant Canty <gc933815@ohio.edu>
2025-07-06 19:18:21 +0200

    able to loop by sub category. creates unique name for each loop. focusing on how to update existing dataframes now to reflect the new data

--- loop_generator.py
+++ loop_generator.py
@@ -1,38 +1,39 @@
 import pandas as pd
 import numpy as np
-import librosa
-import librosa.display
-import json
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.image import resize
-from sklearn.model_selection import train_test_split
-#from tensorflow.keras.models import Sequential, Model
-#from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input, BatchNormalization, Concatenate, GRU, TimeDistributed
-import concurrent.futures
-import time
-from tensorflow.keras.metrics import AUC
 import h5py
-from scipy.signal import butter, filtfilt
-from scipy.ndimage import gaussian_filter1d
 import wave
 import scipy.io.wavfile as wavf
 import os
 
+# i think i messed this up. i need to pass all of the data to the LoopGenerator
+# and then do looping inside. way too confusing to pass one sample at a time inside the generator
+# this lets me update the dataframe and save it once at a time. can also explore parallelism inside
+# the LoopGenerator class
 class LoopGenerator:
-    def __init__(self, swing=.55, velocity_var=.1, note_var=2/12, dir_path="loops"): # swing of .5 is neutral
+    def __init__(self, swing=.55, velocity_var=.1, note_var=2/12, dir_path="loops", bpm_min=70, bpm_max=180): # swing of .5 is neutral
         self.swing = swing
         self.velocity_var = velocity_var
         self.note_var = note_var
         self.dir_path = dir_path
+        self.bpm_min = bpm_min
+        self.bpm_max = bpm_max
+        self.cat_and_sub_cat_id = int(0)
+        self.new_sub_cat_files = []
+        self.new_files = pd.DataFrame()
+        self.old_files = pd.DataFrame()
 
         try:
-            self.df = pd.read_csv('audio_metadata - loops.csv', sep=',')
+            self.existing_files_df = pd.read_csv('audio_metadata - loops.csv', sep=',')
         except:
-            self.df = pd.DataFrame()
-    
+            self.existing_files_df = pd.DataFrame()
+            self.existing_files = False
+            self.id = int(0)
+        else:
+            self.existing_files = True
+            self.id = int(self.existing_files_df['id'].max())
 
-    def safety_guard(self, pattern: list, audio: list, sr: list):
+
+    def safety_guard(self, pattern: list, audio: list, sr: list, category: list, sub_category: list):
         # count of audio files should match count of sample rates
         if len(audio) != len(sr):
             raise Exception('Different counts of audio and sample rates')
@@ -41,43 +42,120 @@ class LoopGenerator:
         if len(np.unique(sr)) > 1:
             raise Exception('Differing sample rates detected')
         sample_rate = sr[0]
+
+        if (len(np.unique(category))) >1:
+            raise Exception('Differing categories detected')
+        cat = category[0]
+        
+        if (len(np.unique(sub_category))) >1:
+            raise Exception('Differeing sub categories detected')
+        sub_cat = sub_category[0]
         
         # pattern cannot be empty
         if len(pattern) < 8:
             raise Exception(f'Pattern is too small. Length of {len(self.pattern)} is < 8 steps')
         
-        return sample_rate
+        return sample_rate, cat, sub_cat
+    
+
+    def bpm(self):
+        return np.random.randint(self.bpm_min, self.bpm_max)
 
 
-    def generate_loop(self, pattern: list, audio: list, sr: list, bpm, category):
-        sample_rate = self.safety_guard(pattern, audio, sr)
+    def generate_loops(self, samples, patterns, num_of_loop_samples, cat):
+        self.old_files = pd.DataFrame()
+        self.new_sub_cat_files= []
+        pat_type = [cat, "Global"]
+        
+        sub_cats = samples['sub_category'].unique()
+        #samples.set_index('id', inplace=True)
+        for sub_cat in sub_cats:
+            self.cat_and_sub_cat_id = 0
+            if pd.isna(sub_cat):
+                samples_split = samples[samples['sub_category'].isna()].copy()
+                if self.existing_files:
+                    vals = self.existing_files_df[self.existing_files_df['file_path'].str.contains(os.path.join(self.dir_path, cat), case=False, na=False)]
+            else:
+                samples_split = samples[samples['sub_category'] == sub_cat].copy()
+                if self.existing_files:
+                    vals = self.existing_files_df[self.existing_files_df['file_path'].str.contains(os.path.join(self.dir_path, cat, sub_cat), case=False, na=False)]
+            
+            samples_split.reset_index(drop=False, inplace=True)
+            if self.existing_files:
+                vals = vals['file_path'].str.split('_', expand=True)
+                vals = vals[len(vals.columns)-1].split('.', expand=True)[0]
+                self.cat_and_sub_cat_id = vals.max()
+            
+            nums = []
+            for index, _ in samples_split.iterrows():
+                nums.append(index)
+                if index % num_of_loop_samples != 0:
+                    pat_choice = np.random.randint(len(pat_type))
+                    which = pat_type[pat_choice]
+
+                    pattern = patterns[which][index % len(patterns[which])]
+                    samples_split.loc[nums[0]:nums[-1]+1, 'Loop_id'] = int(self.id)
+                    rows = samples_split[nums[0]:nums[-1]+1]
+
+                    self.generate_loop(pattern, rows)
+                    nums = []
+            
+            #samples_split.set_index('id', inplace=True)
+
+            #samples.update(samples_split)
+            #self.old_files = samples
+        
+        #print(self.new_files)
+        if isinstance(self.new_sub_cat_files, list):
+            self.new_sub_cat_files = pd.DataFrame(self.new_sub_cat_files)
+        #print(new_files)
+        self.new_files = pd.concat([self.new_files, self.new_sub_cat_files], ignore_index=True)
+
+        #self.old_files = pd.concat([self.old_files, merged], ignore_index=True)
+
+    
+    # edit og dataframe to show the loop id of each sample
+    def generate_loop(self, pattern, rows):
+        audio = rows['waveform'].to_list()
+        sr = rows['sample_rate'].to_list()
+        cats = rows['category'].to_list()
+        sub_cats = rows['sub_category'].to_list()
+        
+        sample_rate, cat, sub_cat = self.safety_guard(pattern, audio, sr, cats, sub_cats)
         
         num_of_beats = 2 ** np.random.randint(2, 5)
+        bpm = self.bpm()
         beat_time = 60 / bpm  # seconds per beat
         loop_duration = beat_time * num_of_beats
         loop_samples = int(np.ceil(loop_duration * sample_rate))
         new_loop = np.zeros(loop_samples, dtype=np.float32)
 
-        for i, hit in enumerate(pattern):
-            if hit > 0:
-                for j in range(0, hit):
-                    velocity = (1-self.velocity_var) * np.random.random_sample() + self.velocity_var
-                    sample_idx = np.random.randint(len(audio))
-                    sample = audio[sample_idx]
+        #ids = rows['id'].to_list()
+        # print(f'number of bars: {int(num_of_beats/4)} cat: {cat} sub_cat: {sub_cat} row_id: {ids} bpm: {bpm}')
+        for bar in range(int(num_of_beats/4)):
+            for i, hit in enumerate(pattern):
+                if hit > 0:
+                    for j in range(0, hit):
+                        velocity = (1-self.velocity_var) * np.random.random_sample() + self.velocity_var
+                        sample_idx = np.random.randint(len(audio))
+                        sample = audio[sample_idx]
+                        
+                        total_step = i + (bar * len(pattern))
+                        start_idx = int((total_step * sample_rate * beat_time / 4) + ((sample_rate / hit) * j))
+                        end_idx = start_idx + len(sample)
+                        
+                        if end_idx > len(new_loop):
+                            diff = end_idx - len(new_loop)
+                            z = np.zeros(diff)
+                            new_loop = np.append(new_loop, z)
 
-                    start_idx = int((i * (sample_rate * beat_time / 4)) + ((sample_rate / hit) * j))
-                    end_idx = start_idx + len(sample)
-                    
-                    if end_idx > len(new_loop):
-                        diff = end_idx - len(new_loop)
-                        z = np.zeros(diff)
-                        new_loop = np.append(new_loop, z)
-
-                    new_loop[start_idx:end_idx] += sample
+                        new_loop[start_idx:end_idx] += sample
         
         new_loop = self.soft_limit(new_loop)
-        self.save_loop(new_loop, category)
-        #return new_loop
+        rows_copy = rows.drop(columns=['waveform', 'sample_rate'], inplace=False)
+        self.save_loop(sample_rate, new_loop, cat, sub_cat, rows_copy)
+        #self.save_dataframe()
+        #print(self.new_files)
 
 
     def soft_limit(self, loop, threshold=.96, max_val=1):
@@ -89,19 +167,59 @@ class LoopGenerator:
         scaled_vals = 1 - np.exp(1)**-zeroed_vals
         scaled_vals = scaled_vals * (max_val - threshold)
         scaled_vals = scaled_vals + threshold
-
         
         loop[over] = sign_x[over] * scaled_vals
         return loop
     
 
-    def save_loop(self, loop, category):
-        print(loop)
-        print(self.dir_path)
-        print(f'/{self.dir_path}')
-        print('')
-        print(os.path.join(self.dir_path, category))
-        os.makedirs(f'{self.dir_path}', exist_ok=True)
+    def save_loop(self, sr, loop, category, sub_category, rows):
+        os.makedirs(self.dir_path, exist_ok=True)
         os.makedirs(os.path.join(self.dir_path, category), exist_ok=True)
+        
+        #print(os.path.join(self.dir_path, category, f'{category}_{id}.wav'))
+        
+        loop = (loop * (2 ** 15 - 1)).astype("<h")
+        if pd.isna(sub_category):
+            #wavf.write(os.path.join(self.dir_path, category, f'{category}_{self.id}.wav'), sr, loop)
+            file_name = f'{category}_{self.cat_and_sub_cat_id}.wav'
+            file_path = os.path.join(self.dir_path, category, file_name)
+            #print(file_path)
+        else:
+            #wavf.write(os.path.join(self.dir_path, category, f'{category}_{sub_category}_{self.id}.wav'), sr, loop)
+            os.makedirs(os.path.join(self.dir_path, category, sub_category), exist_ok=True)
+            file_name = f'{sub_category}_{category}_{self.cat_and_sub_cat_id}.wav'
+            file_path = os.path.join(self.dir_path, category, sub_category, file_name)
+            #print(file_path)
+        
+        
 
-            
+        #rows['Loop_id'] = self.id
+
+        '''new_row = pd.DataFrame(columns=rows.columns)
+        new_row = rows.iloc[0]
+        new_row['id'] = self.id
+        new_row['file_path'] = file_path
+        new_row['file_name'] = file_name
+        new_row.drop(columns=['Loop_id', 'One_Shot_Intent'], inplace=True)'''
+
+        #print(rows)
+        new_row = rows.iloc[0].to_dict()
+        new_row['id'] = self.id
+        new_row['file_path'] = file_path
+        new_row['file_name'] = file_name
+        new_row['One_Shot'] = 0
+        new_row['Loop'] = 1
+        for key in ['Loop_id', 'One_Shot_Intent', 'level_0', 'index']:
+            new_row.pop(key, None)
+
+        #print(new_row)
+        self.new_sub_cat_files.append(new_row)
+
+
+
+        self.id += 1
+        self.cat_and_sub_cat_id += 1
+
+    
+    def save_updates(self):
+        self.new_files.to_csv('audio_metadata - loops.csv', sep=',')
